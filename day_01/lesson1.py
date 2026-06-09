b4ffc85e897324cf3cdcca078e8ec34ff8312f58
Francisco Pablo Marengo <franciscomarengo22@gmail.com>
2026-06-08 21:26:20 -0300

    day 01: arithmetic operators anda data types

---
+++ day_01/lesson1.py
@@ -0,0 +1,18 @@
+print ("Hello World") 
+print(2+2) # addition
+print(3-5) # substraction
+print(2*2) # multiplication
+print(10/2) # division
+print(2**7) # exponential
+print(3%2) # modulus
+print(3//2) # floor division operator
+print('\n"Data Type"\n')
+# checking Data Types
+
+print(type(10))
+print(type(3.14))
+print(type(1 + 3j))
+print(type('Anana'))
+print(type([1,2,3]))
+print(type({'name': 'nana'}))
+print(type({1, 2, 3}))
