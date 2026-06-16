24ccd663caec85d5a9e0d3ee5e620fa47c0261ca
Francisco Pablo Marengo <franciscomarengo22@gmail.com>
2026-06-16 17:49:43 -0300

    day 05: Lists

---
+++ day_05/lesson5.py
@@ -0,0 +1,32 @@
+# Exercises - Day 4
+# Title
+print('\n'+'-'*50,' Exercises - Day 5 ' ,'-'*50 +'\n'+'-'*122)
+
+# List: Is a collection which is ordered and changeable. Allows duplicate members
+
+#1 
+
+empty_list = []
+
+lst = ['element1','element2','element3','element4','element5']
+
+print(len(lst))
+
+print(lst[0])
+
+name = input('Name: ')
+age = input('Age: ')
+height = input('Height: ')
+marit_stat = input('Marital Status: ')
+adress = input('Adress: ')
+mixed_data_types = [name, age, height, marit_stat, adress]
+
+print(mixed_data_types)
+
+companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+print(companies)
+print(len(companies))
+print(companies[0])
+print(companies[-1])
+print(companies[(len(companies)//2)])
