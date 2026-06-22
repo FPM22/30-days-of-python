0d5ba75cb2de9c5e3afdbe5f21e64017484b07fd
Francisco Pablo Marengo <franciscomarengo22@gmail.com>
2026-06-22 18:18:15 -0300

    day 05: lists

--- day_05/rnd_exercises.py
+++ day_05/rnd_exercises.py
@@ -2,6 +2,10 @@ cadena = input('Ingrese una cadena: ')
 print(cadena[::-1])
 
 caracter = input('Ingrese un caracter: ')
+
+while len(caracter) > 1:
+    caracter = input('Ingrese un caracter, no una cadena: ')
+
 contador = 0
 
 for i in range(len(cadena)):
@@ -18,7 +22,7 @@ if len(cadena) == len(cadena2):
     for i in range(len(cadena)):
         if cadena[i] != cadena2[i]:
             distanciaHamming += 1
-    print('Distancia es de: ')
+    print('Distancia es de: ', distanciaHamming)
 else:
     print('Cadenas de distinta longitud.')
 
